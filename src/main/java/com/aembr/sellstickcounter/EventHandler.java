8c9dda22e05c8c173b0c9f5ec3b79cd0f5c26e2c
qrystalz@gmail.com <qrystalz@gmail.com>
2022-11-03 07:47:48 +0000

    Complete core functionality

---
+++ src/main/java/com/aembr/sellstickcounter/EventHandler.java
@@ -0,0 +1,67 @@
+package com.aembr.sellstickcounter;
+
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.inventory.GuiChest;
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.gui.inventory.GuiInventory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import org.lwjgl.input.Keyboard;
+
+import java.util.regex.Matcher;
+
+
+public class EventHandler {
+    @SideOnly(Side.CLIENT)
+    @SubscribeEvent(priority = EventPriority.NORMAL, receiveCanceled = true)
+    public void keyboardEvent(GuiScreenEvent.KeyboardInputEvent.Post event) {
+        GuiScreen screen = event.getGui();
+        if (isValidGui(screen)) {
+            if (Keyboard.getEventKey() == SellstickCounter.countKey && Keyboard.getEventKeyState()) {
+                GuiContainer c = (GuiContainer) screen;
+                int slots = getContainerSlotSize(c);
+                NonNullList<ItemStack> itemStackList = c.inventorySlots.getInventory();
+                int totalUses = 0;
+
+                for (int i = 0; i < slots; i++) {
+                    ItemStack itemStack = itemStackList.get(i);
+                    Item item = itemStack.getItem();
+                    if (item.getUnlocalizedNameInefficiently(itemStack).equals("item.stick")) {
+                        NBTTagList lore = itemStack.getTagCompound().getCompoundTag("display").getTagList("Lore", 8);
+                        for (int j = 0; j < lore.tagCount(); j++) {
+                            String line = lore.getStringTagAt(j);
+                            Matcher matcher = SellstickCounter.searchPattern.matcher(line);
+                            if (matcher.matches()) {
+                                totalUses += Integer.parseInt(matcher.group(1));
+                            }
+                        }
+                    }
+                }
+                Minecraft.getMinecraft().player.sendMessage(new TextComponentString(String.format("%s uses", totalUses)));
+            }
+        }
+    }
+
+    private Boolean isValidGui(GuiScreen screen) {
+        return screen instanceof GuiChest || screen instanceof GuiInventory;
+    }
+
+    private int getContainerSlotSize(GuiContainer screen) {
+        // if chest is open, only count uses inside it
+        if (screen instanceof GuiInventory) {
+            return screen.inventorySlots.inventoryItemStacks.size();
+        } else {
+            return screen.inventorySlots.inventoryItemStacks.size() - 36;
+        }
+    }
+}
---
+++ src/main/java/com/aembr/sellstickcounter/SellstickCounter.java
@@ -0,0 +1,35 @@
+package com.aembr.sellstickcounter;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.lwjgl.input.Keyboard;
+
+import java.util.regex.Pattern;
+
+@Mod(modid = SellstickCounter.MOD_ID, name = SellstickCounter.MOD_NAME, version = SellstickCounter.VERSION)
+public class SellstickCounter {
+    public static final String MOD_ID = "sellstick-counter";
+    public static final String MOD_NAME = "SellStick Counter";
+    public static final String VERSION = "1.0";
+
+    public static int countKey = Keyboard.KEY_P;
+    public static Pattern searchPattern = Pattern.compile("§.(\\d+) §fremaining uses");
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(new EventHandler());
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
